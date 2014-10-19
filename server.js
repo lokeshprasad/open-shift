78d1b58b1e5f059e1ee92d95c134a85af9d63f4f
lokeshprasad <lokesh@lambentgamestudio.com>
2014-10-19 20:02:46 +0530

    added some text

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ require('./config/routes')(app, passport, auth);
 // Start the app by listening on <port>
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log('Express app started on port '+port);
+console.log('My Express app started on port '+port);
 
 // expose app
 exports = module.exports = app;
